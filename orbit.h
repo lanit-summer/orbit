31e6c43ddd54097858e5f4a491cb16f9c642cdf1
spolinaa <sokolova.polina@mail.ru>
2015-09-10 01:53:13 +0300

    Add comments to .h

--- orbit.h
+++ orbit.h
@@ -1,19 +1,22 @@
 #include <vector>
 
+//вектор положения в пространстве
 struct vec
 {
     double x, y, z;
 };
 
+//вектор вращения в пространстве
 struct Rotation
 {
     double rotationAroundX, rotationAroundY, rotationAroundZ;
 };
 
-typedef vec Orient;
-typedef vec Position;
-typedef Rotation RotationSpeed;
+typedef vec Orient; //вектор ориентации в пространстве
+typedef vec Position; //вектор положения в пространстве
+typedef Rotation RotationSpeed; //вектор скорости вращения вокруг трех осей
 
+//начальные положение, ориентация и скорость корабля
 struct ShipPosition
 {
     Position position;
@@ -21,32 +24,41 @@ struct ShipPosition
     vec speedFirst;
 };
 
+//команды, доступные пользователю
 struct PartOfFlightPlan
 {
-    int delayTime;
-    double impulseValue;
-    Rotation rotateValue;
+    int delayTime; //время задержки
+    double impulseValue; //уровень массового расхода топлива
+    Rotation rotateValue; //момент вращения вокруг каждой из осей
 };
 
+//параметры корабля
 struct ShipParams
 {
-    double shipEdgeLength;
-    double shipMass;
-    double fuelMass;
-    Rotation maxRotation;
-    double maxFuelUsagePerSec;
-    double impulsePerFuel;
-    std::vector<PartOfFlightPlan> flightPlan;
-    double maxOverload;
-    double maxHeating;
+    double shipEdgeLength; //длина
+    double shipMass; //масса без топлива
+    double fuelMass; //масса топлива
+    Rotation maxRotation; //максимальные моменты вращения
+    double maxFuelUsagePerSec; //максимальный массовый расход топлива
+    double impulsePerFuel; //удельный импульс двигателя
+    std::vector<PartOfFlightPlan> flightPlan; //массив команд
+    double maxOverload; //предельная перегрузка
+    double maxHeating; //максимальный перегрев
 };
 
 struct Quants
 {
-    int numberOfQuants;
-    double quantSizeOfSec;
+    int numberOfQuants; //количество отрезков времени
+    double quantSizeOfSec; //размер отрезка в секундах
 };
 
-std::vector <Position> computeFlightPlan(ShipPosition initialPosition,
-                                         ShipParams shipParams,
-                                         Quants quants);
+//возвращаемые значения положения и скорости
+struct ReturnValues
+{
+    Position position;
+    vec speed;
+};
+
+std::vector <ReturnValues> computeFlightPlan(ShipPosition initialPosition,
+                                             ShipParams shipParams,
+                                             Quants quants);
