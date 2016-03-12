3be83ced6150c440d87633aa015b1125c1596494
starasov <tarasovsergvas@gmail.com>
2016-03-12 23:19:29 +0300

    Add test project for Orbit.cpp

---
+++ orbit (VS)/OrbitTests/OrbitTests.cpp
@@ -0,0 +1,13 @@
+// OrbitTests.cpp 
+// Tests for functions from Orbit.cpp
+//
+
+#include "stdafx.h"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+
+TEST_CASE("Calculate Aerodynamic heating", "[AerodynamicHeating]") {
+	//Stub on function tests
+	REQUIRE(1==1); 
+}
