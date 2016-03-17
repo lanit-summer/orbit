444ab4b10dd9b0461a4c76002eb017f80cf53536
spolinaa <sokolova.polina@mail.ru>
2016-03-17 22:03:39 +0300

    Added comments and some tests

--- orbit (VS)/OrbitTests/OrbitTests.cpp
+++ orbit (VS)/OrbitTests/OrbitTests.cpp
@@ -15,6 +15,59 @@ TEST_CASE("Calculate Aerodynamic heating", "[AerodynamicHeating]") {
 	REQUIRE(result==100); 
 }
 
+//Angular velocity, result values from Maple 14
+TEST_CASE("Calculate Angular Velocity", "[AngularVelocity]") {
+	//all zero test
+	vec tractiveForce = { 0,0,0 },
+		aeroDynamicForce = { 0,0,0 },
+		gravity = { 0,0,0 },
+		previous = { 0,0,0 };
+	Rotation rotate = { 0,0,0 };
+	double quantSize = 0,
+		length = 0;
+	
+	vec result = { 0,0,0 };
+	REQUIRE(result == calculateAngularVelocity(gravity, aeroDynamicForce, tractiveForce, rotate,
+		quantSize, length, previous));
+
+	//some average values
+	tractiveForce = { 100,200,300 };
+	aeroDynamicForce = { 100,200,300 };
+	gravity = { 100,200,300 };
+	previous = { 100,200,300 };
+	rotate = { 100,200,300 };
+	length = 1000;
+	quantSize = 100;
+	result = { 150100, -149800, 50300 };
+	REQUIRE(result == calculateAngularVelocity(gravity, aeroDynamicForce, tractiveForce, rotate,
+		quantSize, length, previous));
+
+	//big values (max double value C++ ~e308)
+	tractiveForce = { 1e300,1e300,1e300 };
+	aeroDynamicForce = { 1e300,1e300,1e300 };
+	gravity = { 1e300,1e300,1e300 };
+	previous = { 1e300,1e300,1e300 };
+	rotate = { 1e300,1e300,1e300 };
+	length = 1e300;
+	quantSize = 100;
+    result = { 1e300,1e300,1e300 };
+	REQUIRE(result == calculateAngularVelocity(gravity, aeroDynamicForce, tractiveForce, rotate,
+		quantSize, length, previous));
+}
+
+//Check the temperature at 6 km
+TEST_CASE("Calculate temperature", "[temperature]") {
+	double H = 6 + EarthRadius;
+	double result = temperature(H);
+	REQUIRE(result == 255.7); 
+}
+
+//Check the air density at 40 km
+TEST_CASE("Calculate air density", "[airDensity]") {
+	double H = 40 + EarthRadius;
+	double result = airDensity(H);
+	REQUIRE(result ==  0.004);
+}
 
 
 
