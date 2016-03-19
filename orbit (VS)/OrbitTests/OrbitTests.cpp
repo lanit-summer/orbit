0b299eacc9b1723bf670474a2473cdc8634796f8
iskrich <zebster26@mail.ru>
2016-03-19 18:35:00 +0300

    FIX: bug #42

--- orbit (VS)/OrbitTests/OrbitTests.cpp
+++ orbit (VS)/OrbitTests/OrbitTests.cpp
@@ -17,42 +17,48 @@ TEST_CASE("Calculate Aerodynamic heating", "[AerodynamicHeating]") {
 
 //Angular velocity, result values from Maple 14
 TEST_CASE("Calculate Angular Velocity", "[AngularVelocity]") {
-	//all zero test
-	vec tractiveForce = { 0,0,0 },
-		aeroDynamicForce = { 0,0,0 },
-		gravity = { 0,0,0 },
-		previous = { 0,0,0 };
-	Rotation rotate = { 0,0,0 };
-	double quantSize = 0,
-		length = 0;
-	
-	vec result = { 0,0,0 };
-	REQUIRE(result == calculateAngularVelocity(gravity, aeroDynamicForce, tractiveForce, rotate,
-		quantSize, length, previous));
+	SECTION("all zero test") {
+		vec tractiveForce = { 0,0,0 },
+			aeroDynamicForce = { 0,0,0 },
+			gravity = { 0,0,0 },
+			previous = { 0,0,0 };
+		Rotation rotate = { 0,0,0 };
+		double quantSize = 0,
+			length = 0;
 
-	//some average values
-	tractiveForce = { 100,200,300 };
-	aeroDynamicForce = { 100,200,300 };
-	gravity = { 100,200,300 };
-	previous = { 100,200,300 };
-	rotate = { 100,200,300 };
-	length = 1000;
-	quantSize = 100;
-	result = { 150100, -149800, 50300 };
-	REQUIRE(result == calculateAngularVelocity(gravity, aeroDynamicForce, tractiveForce, rotate,
-		quantSize, length, previous));
+		vec result = { 0,0,0 };
+		REQUIRE(result == calculateAngularVelocity(gravity, aeroDynamicForce, tractiveForce, rotate,
+			quantSize, length, previous));
+	}
 
-	//big values (max double value C++ ~e308)
-	tractiveForce = { 1e300,1e300,1e300 };
-	aeroDynamicForce = { 1e300,1e300,1e300 };
-	gravity = { 1e300,1e300,1e300 };
-	previous = { 1e300,1e300,1e300 };
-	rotate = { 1e300,1e300,1e300 };
-	length = 1e300;
-	quantSize = 100;
-    result = { 1e300,1e300,1e300 };
-	REQUIRE(result == calculateAngularVelocity(gravity, aeroDynamicForce, tractiveForce, rotate,
-		quantSize, length, previous));
+	SECTION("some average values") {
+		vec	tractiveForce = { 100,200,300 },
+			aeroDynamicForce = { 100,200,300 },
+			gravity = { 100,200,300 },
+			previous = { 100,200,300 },
+			result = { 150100, -149800, 50300 };
+		Rotation rotate = { 100,200,300 };
+		double	length = 1000,
+			quantSize = 100;
+
+		REQUIRE(result == calculateAngularVelocity(gravity, aeroDynamicForce, tractiveForce, rotate,
+			quantSize, length, previous));
+	}
+
+	SECTION("big values") {
+		//(max double value C++ ~e308)
+		vec	tractiveForce = { 1e300,1e300,1e300 },
+			aeroDynamicForce = { 1e300,1e300,1e300 },
+			gravity = { 1e300,1e300,1e300 },
+			previous = { 1e300,1e300,1e300 },
+			result = { 1e300,1e300,1e300 };
+		Rotation rotate = { 1e300,1e300,1e300 };
+		double	length = 1e300,
+			quantSize = 100;
+
+		REQUIRE(result == calculateAngularVelocity(gravity, aeroDynamicForce, tractiveForce, rotate,
+			quantSize, length, previous));
+	}
 }
 
 //Check the temperature at 6 km
